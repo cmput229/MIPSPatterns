09fe27c50e0ec0e3495dcab640837efa38990ad5
Braedy Kuzma <braedy@ualberta.ca>
2019-01-31 09:23:42 -0700

    Merge main into subroutine section.
    Main is just a subroutine, we can put them together. Also necessitates
    moving everything else "down" one section.

--- explained/03-Subroutines/03b-Main.s
+++ explained/03-Subroutines/03b-Main.s
@@ -6,6 +6,8 @@
 
 # If you are creating a program, not just a function, then you need what is
 # called a program entry point. In C we call this main: in MIPS it is the same.
+# The main function itself is actually just a subroutine that is called by
+# start up code.
 
 # Main must be in a text section since it is code.
 .text
@@ -15,6 +17,10 @@
 main:
   # Main code.
   # ...
+
+  # Main is expecting an integer return. Returning after a successful execution
+  # usually returns 0.
+  li    $v0, 0
 
   # This returns.
   jr    $ra
