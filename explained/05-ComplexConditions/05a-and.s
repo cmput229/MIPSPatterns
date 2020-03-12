c216b6aae21d03fdb0892a22543586dc1610a85c
Braedy Kuzma <braedy@ualberta.ca>
2020-03-12 14:48:46 -0600

    Add better description of conditionals.

--- explained/05-ComplexConditions/05a-and.s
+++ explained/05-ComplexConditions/05a-and.s
@@ -19,22 +19,25 @@ f1:
   # We want something more than a simple condition inside our conditional.
   # Anything more complex than a single boolean expression requires careful
   # understanding of boolean logic and program flow due to how quickly the
-  # header of your loop can expand.
+  # header of a conditional or loop can expand.
 
   # We'll start with AND (&&).
   # Let's check: if ($t0 < 0 AND $t0 != 5) { ... }
-  # If either boolean is false then the body WILL NOT BE executed. This is
-  # called short circuiting.
+  # Booleans are evaluated left to right: first $t0 < 0, next $t0 !=5, and if
+  # there were more then we would continue towards the right. If any boolean is
+  # false then we know that the body will not be executed and do not have to
+  # finish evaluating the other conditions. This is called short circuiting.
 
   # We're doing a simple if with no else here to focus on how to manage more
   # complex conditions. We'll build on this as we go on.
 
-  # Set up for first part of condition would be here, but we don't need any.
+  # Set up for first part of condition would be here, but we don't need any
+  # since the value zero is always in $zero.
 
   # Now check the condition. We want to quit evaluating early (short circuit) if
   # we can. That means if we FAIL the case, then we can skip to the join point.
   # Failing the case here means that t1 >= 0. Given this, we want to BRANCH if
-  # we are GREATER than or EQUAL to (bge).
+  # we are GREATER than or EQUAL to zero (bge).
   bge   $t0, $zero, _f1Join
 
   # If we get here then that means the first condition is true which also means
@@ -42,8 +45,9 @@ f1:
   # Choose $t1, assuming that it's not being used previously.
   li    $t1, 5
 
-  # Now check the condition. We want to fail and go to the join point if this
-  # case fails. We want to BRANCH if $t0 EQUALS 5.
+  # Now check the last condition. We want to fail and go to the join point if
+  # this case fails. The condition $t0 != 5 means we need to BRANCH if $t0
+  # EQUALS 5.
   beq   $t0, $t1, _f1Join
 
   # We're inside the body now, we passed both conditions. Execute your body
